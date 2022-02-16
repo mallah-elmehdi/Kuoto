eedc3edd2c6420841c646b641a6c15ed2f0da96c
mallah-elmehdi <elmehdimallah@gmail.com>
2022-02-16 17:23:53 +0100

    [version 2] kuoto

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ dotenv.config({
   path: "./config.env"
 });
 
-const db = process.env.DATABASE.replace("(PASSWORD)", process.env.DATABASE_PASSWORD);
+const db = process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD);
 mongoose.connect(db, {
   useNewUrlParser: true,
   useCreateIndex: true,
