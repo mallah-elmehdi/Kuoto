fa8a5576bf3db30fa05b66e87a930eb37de51390
mallah-elmehdi <elmehdimallah@gmail.com>
2022-02-18 22:19:01 +0100

    commit test

--- push.sh
+++ push.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
+
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
+
 while true
 do
-        echo "------------PUSHING...------------"
+		echo
+        echo -e "${On_Green}                  PUSHING                  ${Color_Off}"
         git add .
+		echo -n -e "${BPurple}please enter the commit message: ${Color_Off}"
 		read commitmessage
         git commit -m "$commitmessage"
         git push
-        echo "------------FINISHED------------"
-        sleep 10
+        echo -e "${On_Yellow}                  FINISHED : [THE NEXT PUSH IN 5 MINUTES]                  ${Color_Off}"
+        sleep 300
 done
